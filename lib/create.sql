4b0b9a3ede3cc7a387a7fb25a949eefd48d61193
Perry Jorss <pjorss123@gmail.com>
2022-09-26 16:14:16 -0400

    commit tree 33e090e0d2e053b36e77858aa63832c7a9b50a83

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive TEXT
+);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (9, 'Mr. Chocolate', 3, 'M', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (10, 'Rowdy', 3, 'M', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (11, 'Tabitha', 3, 'F', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (12, 'Sergeant Brown', 3, 'M', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (13, 'Melissa', 3, 'F', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (14, 'Grinch', 3, 'M', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (15, 'Wendy', 3, 'F', 'Black', 'goofy', 'yes');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (16, NULL, 3, 'M', 'Black', 'goofy', 'yes');
+
+
